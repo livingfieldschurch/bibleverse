0c1c247bfe08c3fe88b00161cd667088e257a693
livingfieldschurch <livingfields@gmail.com>
2026-08-11 14:49:18 -0400

    Delete living_fields_bible_memory_app/sw.js

--- living_fields_bible_memory_app/sw.js
+++
@@ -1,5 +0,0 @@
-
-const CACHE = "bible-memory-v2";
-const FILES = ["./","./index.html","./styles.css","./verses.js","./manifest.webmanifest","./living-fields-logo.png"];
-self.addEventListener("install", e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES))));
-self.addEventListener("fetch", e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
